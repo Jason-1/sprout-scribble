087b08437b87fedc88cafc73830cb07230b53116
Jason-1 <jason722698@gmail.com>
2024-10-09 17:51:08 +1300

    Created DB connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
+export default async function Home() {
 
-export default function Home() {
   return (
     <main>
-      <h1>Welcom to Next.js</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={72}/>
+      <div>{Date.now()}</div>
     </main>
   );
 }
