458e3f56c0b71f3abe47e8834c65e05e22c91afa
Jason-1 <jason722698@gmail.com>
2024-10-14 12:19:23 +1300

    Back on track

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
-import { profile } from "console";
+
+import Nav from "@/components/navigation/nav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Nav />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default async function Home() {
   return (
     <main>
       <h1>Homepage</h1>
-      <Button variant={"destructive"}>Click me</Button>
     </main>
   );
 }
