1d9e6fc33a90abb4c5745eecd509aa555f2510dc
Jason-1 <jason722698@gmail.com>
2024-10-18 00:04:28 +1300

    16

--- server/actions/email-register.ts
+++ server/actions/email-register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 import { db } from "..";
 import { eq } from "drizzle-orm";
 import { users } from "../schema";
+import { generateEmailVerificationToken } from "./tokens";
 
 const action = createSafeActionClient();
 
@@ -13,21 +14,34 @@ export const emailRegister = action
   .schema(RegisterSchema)
   .action(async ({ parsedInput }) => {
     const { email, name, password } = parsedInput;
-
+    //hashing passwords
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
 
+    //check exisitng user
     const existingUser = await db.query.users.findFirst({
       where: eq(users.email, email),
     });
 
-    //check if email is in DB, if so sat its in use, if not register user and send verification email
-
     if (existingUser) {
-      // if(!existingUser.emailVerified) {
-      //     const verificationToken =
-      // }
+      if (!existingUser.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(email);
+        //await setVerificationEmail();
+
+        return { success: "Email Confirmation resent" };
+      }
       return { error: "Email already in use" };
     }
+
+    //logic for when user not registered
+
+    await db.insert(users).values({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    const verificationToken = await generateEmailVerificationToken(email);
+    //await setVerificationEmail();
+
     return { success: "Register successful" };
   });
