00acc5855f83cf55a935881320b365381cfefddf
Jason-1 <jason722698@gmail.com>
2024-11-27 14:29:38 +1300

    Cart working

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Logo from "@/components/navigation/logo";
 import { LogIn } from "lucide-react";
 import { Button } from "../ui/button";
+import CartDrawer from "../cart/cart-drawer";
 
 export default async function Nav() {
   const session = await auth();
@@ -11,15 +12,17 @@ export default async function Nav() {
   return (
     <header className="py-8">
       <nav>
-        <ul className="flex justify-between items-center">
-          <li>
+        <ul className="flex justify-between items-center md:gap-8 gap-4">
+          <li className="flex flex-1">
             <Link href="/" aria-label="sprout and scribble logo">
               <Logo />
             </Link>
           </li>
-
+          <li className="relative flex items-center hover:bg-muted">
+            <CartDrawer />
+          </li>
           {!session ? (
-            <li>
+            <li className="flex items-center justify-center">
               <Button asChild>
                 <Link
                   className="flex gap-2"
@@ -32,7 +35,7 @@ export default async function Nav() {
               </Button>
             </li>
           ) : (
-            <li>
+            <li className="flex items-center justify-center">
               <UserButton expires={session?.expires} user={session?.user} />
             </li>
           )}
