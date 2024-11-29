26014ebec731641b1b40ab5c91db7c2eb93471cf
Jason-1 <jason722698@gmail.com>
2024-11-29 14:57:50 +1300

    Order saved to DB

--- components/cart/cart-drawer.tsx
+++ components/cart/cart-drawer.tsx
@@ -14,7 +14,7 @@ import CartMessage from "./cart-message";
 import Payment from "./payment";
 
 export default function CartDrawer() {
-  const { cart, checkoutProgress } = useCartStore();
+  const { cart, checkoutProgress, setCheckoutProgress } = useCartStore();
   return (
     <Drawer>
       <DrawerTrigger>
@@ -41,6 +41,11 @@ export default function CartDrawer() {
         <div className="overflow-auto p-4">
           {checkoutProgress === "cart-page" && <CartItems />}
           {checkoutProgress === "payment-page" && <Payment />}
+          {checkoutProgress === "confirmation-page" && (
+            <div onClick={() => setCheckoutProgress("cart-page")}>
+              Confirmation
+            </div>
+          )}
         </div>
       </DrawerContent>
     </Drawer>
--- components/cart/payment-form.tsx
+++ components/cart/payment-form.tsx
@@ -10,13 +10,29 @@ import {
 import { Button } from "../ui/button";
 import { useState } from "react";
 import { createPaymentIntent } from "@/server/actions/create-payment-intent";
+import { useAction } from "next-safe-action/hooks";
+import { createOrder } from "@/server/actions/create-order";
+import { toast } from "sonner";
 
 export default function PaymentForm({ totalPrice }: { totalPrice: number }) {
   const stripe = useStripe();
   const elements = useElements();
-  const { cart } = useCartStore();
+  const { cart, setCheckoutProgress } = useCartStore();
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const { execute } = useAction(createOrder, {
+    onSuccess: (data) => {
+      if (data.data?.error) {
+        toast.error(data.data.error);
+      }
+      if (data.data?.success) {
+        setIsLoading(false);
+        toast.success(data.data.success);
+        setCheckoutProgress("confirmation-page");
+      }
+    },
+  });
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -31,8 +47,8 @@ export default function PaymentForm({ totalPrice }: { totalPrice: number }) {
       setIsLoading(false);
       return;
     }
-    const { data } = await createPaymentIntent({
-      amount: totalPrice,
+    const paymentIntentResult = await createPaymentIntent({
+      amount: totalPrice * 100,
       currency: "usd",
       cart: cart.map((item) => ({
         quantity: item.variant.quantity,
@@ -42,19 +58,19 @@ export default function PaymentForm({ totalPrice }: { totalPrice: number }) {
         image: item.image,
       })),
     });
-    if (data?.error) {
-      setErrorMessage(data.error);
+    if (paymentIntentResult?.data?.error) {
+      setErrorMessage(paymentIntentResult.data.error);
       setIsLoading(false);
       return;
     }
-    if (data?.success) {
+    if (paymentIntentResult?.data?.success) {
       const { error } = await stripe.confirmPayment({
         elements,
-        clientSecret: data.success.clientSecretID!,
+        clientSecret: paymentIntentResult.data?.success.clientSecretID!,
         redirect: "if_required",
         confirmParams: {
           return_url: "http://localhost:3000/success",
-          receipt_email: data.success.user as string,
+          receipt_email: paymentIntentResult.data?.success.user as string,
         },
       });
       if (error) {
@@ -63,17 +79,27 @@ export default function PaymentForm({ totalPrice }: { totalPrice: number }) {
         return;
       } else {
         setIsLoading(false);
-        console.log("save the order");
+        execute({
+          status: "pending",
+          total: totalPrice,
+          products: cart.map((item) => ({
+            productID: item.id,
+            variantID: item.variant.variantID,
+            quantity: item.variant.quantity,
+          })),
+        });
       }
     }
   };
-
   return (
     <form onSubmit={handleSubmit}>
       <PaymentElement />
       <AddressElement options={{ mode: "shipping" }} />
-      <Button disabled={!stripe || !elements}>
-        <span>Pay now</span>
+      <Button
+        className="my-4 w-full"
+        disabled={!stripe || !elements || isLoading}
+      >
+        <span>{isLoading ? "processing" : "Pay now"}</span>
       </Button>
     </form>
   );
