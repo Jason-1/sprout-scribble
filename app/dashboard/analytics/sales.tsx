a6bc579ff15c3a5dde7500cc9245c8688d8d3cf9
Jason-1 <jason722698@gmail.com>
2024-12-11 12:54:55 +1300

    Analytics

---
+++ app/dashboard/analytics/sales.tsx
@@ -0,0 +1,91 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHeader,
+  TableRow,
+  TableFooter,
+  TableHead,
+} from "@/components/ui/table";
+import { TotalOrders } from "@/lib/infer-types";
+import Image from "next/image";
+import placeholderUser from "@/public/placeholder-user.jpg";
+
+export default function Sales({ totalOrders }: { totalOrders: TotalOrders[] }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>New Sales</CardTitle>
+        <CardDescription>History of your recent sales</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Item</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Image</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {totalOrders.map(
+              ({ order, product, quantity, productVariants }) => (
+                <TableRow className="font-medium" key={order.id}>
+                  <TableCell>
+                    {order.user.image && order.user.name ? (
+                      <div className="flex gap-2 w-32 items-center">
+                        <Image
+                          src={order.user.image}
+                          width={25}
+                          height={25}
+                          alt={order.user.name}
+                          className="rounded-full"
+                        />
+                        <p className="text-xs font-medium">{order.user.name}</p>
+                      </div>
+                    ) : (
+                      <div className="flex gap-2 items-center">
+                        <Image
+                          src={placeholderUser}
+                          width={25}
+                          height={25}
+                          alt="User not found"
+                          className="rounded-full"
+                        />
+                        <p className="text-xs font-medium">User not found</p>
+                      </div>
+                    )}
+                  </TableCell>
+                  <TableCell>{product.title}</TableCell>
+                  <TableCell>{product.price}</TableCell>
+                  <TableCell>{quantity}</TableCell>
+                  <TableCell>
+                    <Image
+                      src={productVariants.variantImages[0].url}
+                      width={25}
+                      height={25}
+                      alt={product.title}
+                      className="rounded-full"
+                    />
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
--- lib/infer-types.ts
+++ lib/infer-types.ts
@@ -47,3 +47,12 @@ export type ReviewsWithUser = InferResultType<
     user: true;
   }
 >;
+
+export type TotalOrders = InferResultType<
+  "orderProduct",
+  {
+    order: { with: { user: true } };
+    product: true;
+    productVariants: { with: { variantImages: true } };
+  }
+>;
